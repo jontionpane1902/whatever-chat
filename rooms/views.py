5456643c3ef6aba81a4be280470beee06edc688f
Chayut <chayut.jj@gmail.com>
2022-10-05 00:50:18 +0700

    add todo list

--- rooms/views.py
+++ rooms/views.py
@@ -5,4 +5,5 @@ from django.core.handlers.wsgi import WSGIRequest
 def index(request: WSGIRequest):
     if not request.session.session_key:
         return redirect('main')
+    # TODO: Fetch room list
     return render(request, 'rooms/base.html')

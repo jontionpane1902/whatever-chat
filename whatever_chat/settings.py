6498c1f69499342259961c393665e42ea58c69ae
Chayut <chayut.jj@gmail.com>
2022-11-04 14:22:42 +0700

    hide secret key

--- whatever_chat/settings.py
+++ whatever_chat/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6fu9g!g1a7^lbpy2&-g!=w7#$1ug^gjfs!x5(z^)&w45-zz6_%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
